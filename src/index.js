cd5bcf653d53a76960beeefe531ee1569a853301
Lukasz Kurek <lukascoorek@gmail.com>
2019-11-21 11:30:01 +0100

    User query & fragments

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
 import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
-import Query from "./resolvers/Query";
-import Mutation from "./resolvers/Mutation";
-import Subscription from "./resolvers/Subscription";
-import Post from "./resolvers/Post";
-import User from "./resolvers/User";
-import Comment from "./resolvers/Comment";
+import { resolvers, fragmentReplacements } from "./resolvers/index";
 import prisma from "./prisma";
 
 const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
+  resolvers,
   context(request) {
     return { db, pubsub, prisma, request };
   },
-  resolvers: {
-    Query,
-    Mutation,
-    Subscription,
-    Post,
-    User,
-    Comment
-  }
+  fragmentReplacements
 });
 
 server.start(() => console.log("Server is up"));
