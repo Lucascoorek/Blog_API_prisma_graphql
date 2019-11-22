7eede06176ecc169388813b9aa606bab4d89a8c8
Lukasz Kurek <lukascoorek@gmail.com>
2019-11-22 12:05:49 +0100

    auth for subscription

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -1,7 +1,9 @@
 import jwt from "jsonwebtoken";
 
 const getUserId = (request, requireAuth = true) => {
-  const headers = request.request.headers.authorization;
+  const headers = request.request
+    ? request.request.headers.authorization
+    : request.connection.context.Authorization;
 
   if (headers) {
     const token = headers.replace("Bearer ", "");
